0f7412d474d2fb27ce515c10f54fb947a6803e34
pureNoi <74644255+pureNoi@users.noreply.github.com>
2024-07-31 02:04:09 +0800

    Passed All Tests Without Processing Memory Leak Issue

--- LinkedListStack/Stack.cpp
+++ LinkedListStack/Stack.cpp
@@ -1,1 +1,66 @@
 #include "Stack.h"
+
+void Stack::Push(int val)
+{	
+	if(ElementNum == 0)
+	{
+		FirstElementPtr = new Element(val, nullptr);
+		FinalElementPtr = FirstElementPtr;
+	}
+	else
+	{	
+		Element* TempFinalElementPtr = FinalElementPtr;
+		FinalElementPtr = new Element(val, TempFinalElementPtr);
+		TempFinalElementPtr->SetNextElementPtr(FinalElementPtr);
+	}
+	ElementNum++;
+}
+
+int Stack::Pop()
+{	
+	if (ElementNum == 0)
+	{
+		return -1;
+	}
+	int FinalElementValue = FinalElementPtr->GetElementValue();
+	if (ElementNum == 1) 
+	{	
+		delete FirstElementPtr;
+		FirstElementPtr = nullptr;
+		ElementNum--;
+	}
+	else
+	{
+		Element* TempLastElementPtr = FinalElementPtr->GetLastElementPtr();
+		delete FinalElementPtr;
+		TempLastElementPtr->SetNextElementPtr(nullptr);
+		FinalElementPtr = TempLastElementPtr;
+		ElementNum--;
+	}
+	return FinalElementValue;
+}
+
+int Stack::Size() const
+{	
+	/*int ElementCount = 0;
+	if (!FirstElementPtr)
+	{
+		return ElementCount;
+	}
+	else
+	{
+		Element* CurrentElementPtr = FirstElementPtr;
+		do
+		{
+			ElementCount++;
+			CurrentElementPtr = CurrentElementPtr->GetNextElementPtr();
+		} while (CurrentElementPtr);
+	}
+	return ElementCount;*/
+	return ElementNum;
+}
+
+bool Stack::Empty() const
+{
+	return !FirstElementPtr;
+}
--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -7,4 +7,75 @@ public:
 	int Pop();
 	int Size() const;
 	bool Empty() const;
+	Stack() = default;
+	Stack(const Stack& StackInput)
+	{
+		*this = StackInput;
+	};
+
+	Stack& operator=(const Stack& StackInput)
+	{	
+		if (this == &StackInput) {
+			return *this;  // 处理自赋值
+		}
+
+		// 释放当前资源
+		while (FirstElementPtr) {
+			Element* temp = FirstElementPtr;
+			FirstElementPtr = FirstElementPtr->GetNextElementPtr();
+			delete temp;
+		}
+
+		Element* InputCurrentElementPtr = StackInput.FirstElementPtr;
+		this->FirstElementPtr = new Element(InputCurrentElementPtr->GetElementValue(), nullptr);
+		Element* CurrentElementPtr = this->FirstElementPtr;
+
+		while (InputCurrentElementPtr->GetNextElementPtr())
+		{	
+			InputCurrentElementPtr = InputCurrentElementPtr->GetNextElementPtr();
+			CurrentElementPtr = new Element(InputCurrentElementPtr->GetElementValue(), CurrentElementPtr);
+		}
+		this->FinalElementPtr = CurrentElementPtr;
+		this->ElementNum = StackInput.ElementNum;
+		return *this;
+	};
+
+public:
+	class Element 
+	{
+	public:
+		Element(int ValueInput, Element* LastElementPtrInput) :
+		Value(ValueInput),
+		LastElementPtr(LastElementPtrInput)
+		{}
+
+		void SetNextElementPtr( Element* NextElementPtrInput )
+		{
+			NextElementPtr = NextElementPtrInput;
+		}
+
+		int GetElementValue() 
+		{
+			return Value;
+		}
+
+		Element* GetLastElementPtr()
+		{
+			return LastElementPtr;
+		}
+
+		Element* GetNextElementPtr()
+		{
+			return NextElementPtr;
+		}
+
+	private:
+		int Value;
+		Element* LastElementPtr = nullptr;
+		Element* NextElementPtr = nullptr;
+	};
+private:
+	Element* FirstElementPtr = nullptr;
+	Element* FinalElementPtr = nullptr;
+	int ElementNum = 0;
 };
